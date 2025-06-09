fcd6c66fd3742631b4b1493670336468ccb0aaf7
leonqiann <leonqian@riccarton.school.nz>
2025-06-10 08:37:30 +1200

    added text to the menu

--- sql_assessment.py
+++ sql_assessment.py
@@ -41,11 +41,11 @@ menu_choice = ''
 while menu_choice != 'Z':
     menu_choice = input('Welcome to the cars database\n\n'
                         'Type the letter for the information you want\n'
-                        "A: \n"
-                        "B: \n"
-                        'C: \n'
-                        'D: \n'
-                        'E: \n'
+                        "A: Top 3 scorers\n"
+                        "B: Top 3 rebounders\n"
+                        'C: top 3 assisters\n'
+                        "D: Info for player who aren't from the USA \n"
+                        'E: Info about a player with a specific jersey number\n'
                         'F: \n'
                         'G: \n'
                         'H: \n'
@@ -63,7 +63,7 @@ while menu_choice != 'Z':
     elif menu_choice == 'E':
         jersey_num = int(input('Enter a jersey number to see which player has that specific jersey number: '))
         print_parameter_query("first_name, last_name, position, age, height, weight, ppg, rpg, apg, jersey_num, country", "jersey_num = ? ORDER BY ppg DESC",jersey_num)
-
+        
     elif menu_choice == 'H':
         position = input('Which position do you want to see: ')
         print_parameter_query("first_name, last_name, position, age, height, weight, ppg, rpg, apg, jersey_num, country", "position = ? ORDER BY ppg DESC",position)
